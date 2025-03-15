9fa69107a4bc5506837b0bb200b59f5657f4d27f
AllanWinata <allan.winata@cakrawala.ac.id>
2025-03-15 23:43:59 +0700

    Delete Tugas 2/Cek bilangan Genap atau Ganjil.js

--- Tugas 2/Cek bilangan Genap atau Ganjil.js
+++
@@ -1,7 +0,0 @@
-function isEven(n) {
-    return n % 2 === 0;
-}
-
-// Contoh penggunaan
-console.log(`1000 -> isEven: ${isEven(1000)}`); // Output: true
-console.log(`1001 -> isEven: ${isEven(1001)}`); // Output: false
