f4dc3bede8d75baa847a468199790991b4347398
AllanWinata <allan.winata@cakrawala.ac.id>
2025-03-15 23:25:36 +0700

    Create Convert Centimeter to Kilometer.js

---
+++ Tugas 2 /Convert Centimeter to Kilometer.js
@@ -0,0 +1,11 @@
+function convertLength(value) {
+    if (value >= 100000) {
+        return `${value / 100000}km`;
+    } else {
+        return `${value * 100000}cm`;
+    }
+}
+
+// Contoh penggunaan
+console.log(convertLength(100000)); // Output: "1km"
+console.log(convertLength(1));      // Output: "100000cm"
