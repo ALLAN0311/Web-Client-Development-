e6ab5682e896a3bd435ba568ee59c3ec20eb8c08
AllanWinata <allan.winata@cakrawala.ac.id>
2025-03-15 23:58:09 +0700

    Delete Tugas 2 /Kemunculan Pertama dari string.js

--- Tugas 2 /Kemunculan Pertama dari string.js
+++
@@ -1,6 +0,0 @@
-function removeFirstOccurrence(str, searchString) {
-    return str.replace(searchString, '');
-}
-
-// Contoh penggunaan
-console.log(removeFirstOccurrence("Hello World", "ell")); // Output: "Ho World"
